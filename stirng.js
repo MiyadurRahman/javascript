15d43fe2fd6d9c99cb6df3ff830150104929f249
Meyadur Rahman <100347980+MiyadurRahman@users.noreply.github.com>
2026-08-31 00:45:13 +0600

    basics

---
+++ stirng.js
@@ -0,0 +1,14 @@
+let firstname="miyad";
+let lastname="rahman";
+// let fullname=firstname+" "+lastname;
+
+// let fullname='firstname +" "+ lastname';
+
+let fullname='      ${firstname} ${lastname}';
+
+
+console.log(fullname);
+
+let randomvar="i am a studen";
+console.log(randomvar.toUpperCase());
+console.log(randomvar.includes());
---
+++ variables.js
@@ -0,0 +1,21 @@
+console.log("hello world");
+let name="Rahim";
+// name="karim";
+console.log(name);
+
+ const age=21;//we cannot reasign when using const
+// age=20;
+console.log(age);
+
+console.log(typeof(name),typeof (age));
+
+
+var year=parseInt('2020');
+console.log(typeof(year))
+
+var first=0.1;
+var second=0.2;
+var total=first +second;
+console.log(total);
+console.log(total.toFixed(5));
+// name convatio
