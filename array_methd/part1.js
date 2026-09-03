b89b3f043ddb974693b9cee3c8e2a43587180bd4
Meyadur Rahman <100347980+MiyadurRahman@users.noreply.github.com>
2026-09-03 12:22:17 +0600

    array method

---
+++ array_methd/part1.js
@@ -0,0 +1,38 @@
+// foreach,map
+
+//foreach ,from foreach we cannot return
+let fruits=["Apple","Banna","mango "]
+
+let newFruits = fruits.forEach((fruits,index)=>{
+console.log(`${index+1} --> ${fruits}`);
+// return `${index+1} --> ${fruits}` cannot return
+
+
+
+})
+
+
+//map ,returns
+
+
+
+let newFruits2 = fruits.map((fruits,index)=> {
+    
+    return  `${index+1} --> ${fruits}`;
+
+
+    
+});
+
+console.log(newFruits2);
+//to upper cae
+let newfruits3=fruits.map((fruits)=>fruits.toUpperCase());
+
+console.log(newfruits3);
+
+
+
+// filter
+let newfruits4=fruits.filter((fruits)=>5<fruits.length);
+
+console.log(newfruits4);
